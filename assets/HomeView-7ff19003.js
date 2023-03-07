0884ab60e315b0cd4683c13a0cb921572f558cb3
Barret <jianhong.liou@gmail.com>
2023-03-08 00:26:14 +0800

    deploy

---
+++ assets/HomeView-7ff19003.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,c as t,a as e}from"./index-c9e35247.js";const c={},n={class:"home"},_=e("h1",null,"Hello, This is Home Page.",-1),a=e("hr",null,null,-1),l=[_,a];function r(i,d){return s(),t("div",n,l)}const m=o(c,[["render",r]]);export{m as default};
