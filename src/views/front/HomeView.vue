95a6014b31263da4aa2567a5c4118250d9032ecf
Barret <jianhong.liou@gmail.com>
2023-03-10 23:20:58 +0800

    deploy

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -11,7 +11,9 @@
     </div>
   </main>
   <!-- scroll down -->
-  <section class="vh-100 d-none d-lg-flex justify-content-center align-items-end">
+  <section
+    class="vh-100 d-none d-lg-flex justify-content-center align-items-end"
+  >
     <div class="d-flex justify-content-center align-items-center mb-72">
       <span class="border-start-12"></span>
       <p class="text-secondary fs-5 font-monospace text-uppercase px-24 mb-0">
@@ -62,44 +64,71 @@
         </h1>
       </div>
       <div class="row">
-        <div class="col d-flex justify-content-center flex-column align-items-center">
+        <div
+          class="col d-flex justify-content-center flex-column align-items-center"
+        >
           <div class="position-relative mb-32">
             <div class="border-top-104"></div>
-            <button type="button"
-              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64">
+            <button
+              type="button"
+              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+            >
               招牌漢堡
             </button>
           </div>
-          <img src="/src/assets/images/product-1.jpg" class="img-fluid rounded rounded-5 mb-32" alt="" srcset="" />
-          <p class="text-center">
+          <img
+            src="/src/assets/images/product-1.jpg"
+            class="img-fluid rounded rounded-5 mb-32"
+            alt=""
+            srcset=""
+          />
+          <p class="text-center px-24">
             最經典的美味，小時候老爸都會說，去找你阿母拿一個漢堡蛋當早餐
           </p>
           <div class="border-bottom-104"></div>
         </div>
-        <div class="col d-flex justify-content-center flex-column align-items-center">
+        <div
+          class="col d-flex justify-content-center flex-column align-items-center"
+        >
           <div class="position-relative mb-32">
             <div class="border-top-104"></div>
-            <button type="button"
-              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64">
+            <button
+              type="button"
+              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+            >
               嚴選套餐
             </button>
           </div>
-          <img src="/src/assets/images/product-2.jpg" class="img-fluid rounded rounded-5 mb-32" alt="" srcset="" />
-          <p class="text-center">
+          <img
+            src="/src/assets/images/product-2.jpg"
+            class="img-fluid rounded rounded-5 mb-32"
+            alt=""
+            srcset=""
+          />
+          <p class="text-center px-24">
             這個也想吃、那個也想吃？那就來個套餐滿足慾望
           </p>
           <div class="border-bottom-104"></div>
         </div>
-        <div class="col d-flex justify-content-center flex-column align-items-center">
+        <div
+          class="col d-flex justify-content-center flex-column align-items-center"
+        >
           <div class="position-relative mb-32">
             <div class="border-top-104"></div>
-            <button type="button"
-              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64">
+            <button
+              type="button"
+              class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+            >
               滑嫩蛋餅
             </button>
           </div>
-          <img src="/src/assets/images/product-3.jpg" class="img-fluid rounded rounded-5 mb-32" alt="" srcset="" />
-          <p class="text-center">
+          <img
+            src="/src/assets/images/product-3.jpg"
+            class="img-fluid rounded rounded-5 mb-32"
+            alt=""
+            srcset=""
+          />
+          <p class="text-center px-24">
             傳統的蛋餅皮，換上Q彈滑嫩的河粉餅皮，蹦出新滋味！
           </p>
           <div class="border-bottom-104"></div>
@@ -107,4 +136,33 @@
       </div>
     </section>
   </div>
+  <!-- make life smoother -->
+  <section class="mt-200 position-relative">
+    <div class="d-flex justify-content-end">
+      <img
+        src="/src/assets/images/hero2.png"
+        style="margin-right: 110.8px"
+        alt=""
+      />
+    </div>
+    <div class="w-100 h-100 position-absolute top-0 start-0">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+          <div class="col-5">
+            <h2 class="mb-8 ls-2">你的煩腦，</h2>
+            <h2 class="mb-0 ls-2">我也許能幫忙！</h2>
+            <h1 class="text-secondary font-monospace text-uppercase mb-48">
+              make life smoother
+            </h1>
+            <p class="fs-6 ls-15 mb-48">
+              每天早上都要煩惱吃什麼嗎？除了日復一日的餐點，來試試樂透的早餐吧！
+            </p>
+            <a name="" id="" class="btn btn-secondary rounded-pill fs-6 ls-3 px-32 py-24" href="#" role="button"
+              >讓我幫你選早餐吧</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
