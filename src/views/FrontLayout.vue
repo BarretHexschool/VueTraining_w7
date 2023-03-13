cbca93c96c716de55458dbc994a4b1042bf6cf34
Barret <jianhong.liou@gmail.com>
2023-03-13 12:50:33 +0800

    deploy-fix RWD

--- src/views/FrontLayout.vue
+++ src/views/FrontLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-primary  position-absolute zn-0 w-100">
   <div class="position-absolute z-3 w-100">
-    <div class="container py-32">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary py-24 bg-none">
+    <div class="container py-4">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary py-3 bg-none">
         <router-link class="nav-link" to="/"
           ><img
             src="/src/assets/images/logo.png"
@@ -44,13 +44,13 @@
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
             <li class="nav-item rounded rounded-pill">
               <router-link
-                class="nav-link fs-6 fs-6-btn ls-3 font-black py-16 px-32"
+                class="nav-link fs-6 fs-6-btn ls-3 font-black py-2 px-4"
                 to="/products"
                 >鮮堡美食</router-link
               >
             </li>
             <li class="nav-item rounded rounded-pill">
-              <router-link class="nav-link fs-6 fs-6-btn ls-3 py-16 px-32" to="/about"
+              <router-link class="nav-link fs-6 fs-6-btn ls-3 py-2 px-4" to="/about"
                 >關於我們</router-link
               >
             </li>
@@ -59,17 +59,17 @@
             >
               <router-link
                 to="/cart"
-                class="btn d-flex justify-content-center align-items-center ls-3 py-16 px-32 bg-secondary rounded-pill"
+                class="btn d-flex justify-content-center align-items-center ls-3 py-2 px-4 bg-secondary rounded-pill"
               >
                 <img
                   src="/src/assets/images/icon-cart.png"
-                  class="me-8 hover-hide"
+                  class="me-1 hover-hide"
                   alt=""
                   srcset=""
                 />
                 <img
                   src="/src/assets/images/icon-cart-hover.png"
-                  class="me-8 hover"
+                  class="me-1 hover"
                   alt=""
                   srcset=""
                 />
@@ -83,6 +83,12 @@
     </div>
   </div>
   <router-view></router-view>
+  <!-- footer -->
+  <footer class="mt-200 container mt-sm-80">
+    <div class="footer">
+      <p class="text-center fs-7 ls-15 mb-0">© 2023 鮮堡漢堡 板橋文化店 版權所有</p>
+    </div>
+  </footer>
 </div>
 </template>
 <script>
--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -1,12 +1,25 @@
 <template>
   <section class="position-absolute z-1 w-100">
-    <img src="/src/assets/images/banner-d.png" class="w-100" alt="" />
+    <img
+      src="/src/assets/images/banner-d.png"
+      class="w-100 d-none d-md-inline"
+      alt=""
+    />
+    <img
+      src="/src/assets/images/banner-m.png"
+      class="w-100 img-fluid d-md-none overflow-hidden"
+      alt=""
+    />
   </section>
-  <main class="position-absolute w-100 mt-355 z-2">
+  <main class="position-absolute w-100 mt-355 z-2 mt-sm-184">
     <div class="container">
       <div class="col-6">
-        <img src="/src/assets/images/hero_word.png" class="mb-32" alt="" />
-        <h2 class="ls-2">活力早餐的加油站</h2>
+        <img
+          src="/src/assets/images/hero_word.png"
+          class="mb-4 img-fluid"
+          alt=""
+        />
+        <h2 class="ls-2 text-nowrap">活力早餐的加油站</h2>
       </div>
     </div>
   </main>
@@ -16,133 +29,149 @@
   >
     <div class="d-flex justify-content-center align-items-center mb-72">
       <span class="border-start-12"></span>
-      <p class="text-secondary fs-5 font-monospace text-uppercase px-24 mb-0">
+      <p class="text-secondary fs-5 font-monospace text-uppercase px-3 mb-0">
         scroll down
       </p>
       <span class="border-end-12"></span>
     </div>
   </section>
 
-  <div class="mt-500 container">
+  <div class="mt-500 mt-sm-860 container">
     <!-- latest news -->
-    <section class="row">
-      <div class="col-5">
-        <h2 class="ls-2 mb-0">最新消息</h2>
-        <h1 class="text-secondary font-monospace text-uppercase">
+    <section class="row flex-sm-row flex-column">
+      <div class="col-sm-5 col mb-sm-0 mb-4">
+        <h2 class="ls-2 mb-sm-0 mb-1 text-sm-start text-center">最新消息</h2>
+        <h2
+          class="fs-1 text-secondary font-monospace text-uppercase text-sm-start text-center"
+        >
           latest news
-        </h1>
+        </h2>
       </div>
-      <div class="col-7">
-        <div class="d-flex justify-content-between align-items-center mb-48">
+      <div class="col-sm-7 col">
+        <div
+          class="d-flex justify-content-between align-items-center mb-sm-6 mb-3"
+        >
           <span class="border-start-16"></span>
-          <p class="fs-5 px-24 mb-0 ls-15">112 年春節 初一～初六休息</p>
+          <p class="fs-5 px-3 mb-0 ls-15">112 年春節 初一～初六休息</p>
           <span class="border-end-16 ms-auto"></span>
         </div>
-        <div class="d-flex justify-content-between align-items-center mb-48">
+        <div
+          class="d-flex justify-content-between align-items-center mb-sm-6 mb-3"
+        >
           <span class="border-start-16"></span>
-          <p class="fs-5 px-24 mb-0 ls-15"></p>
+          <p class="fs-5 px-3 mb-0 ls-15"></p>
           <span class="border-end-16 ms-auto"></span>
         </div>
-        <div class="d-flex justify-content-between align-items-center mb-48">
+        <div
+          class="d-flex justify-content-between align-items-center mb-sm-6 mb-3"
+        >
           <span class="border-start-16"></span>
-          <p class="fs-5 px-24 mb-0 ls-15"></p>
+          <p class="fs-5 px-3 mb-0 ls-15"></p>
           <span class="border-end-16 ms-auto"></span>
         </div>
-        <div class="d-flex justify-content-between align-items-center mb-48">
+        <div
+          class="d-flex justify-content-between align-items-center mb-sm-6 mb-3"
+        >
           <span class="border-start-16"></span>
-          <p class="fs-5 px-24 mb-0 ls-15"></p>
+          <p class="fs-5 px-3 mb-0 ls-15"></p>
           <span class="border-end-16 ms-auto"></span>
         </div>
       </div>
     </section>
     <!-- start from here -->
-    <section class="mt-200">
-      <div class="d-flex flex-column align-items-end mb-96">
-        <h2 class="ls-2 mb-0">活力早晨，從這開始！</h2>
-        <h1 class="text-secondary font-monospace text-uppercase">
+    <section class="mt-200 mt-sm-80">
+      <div class="d-flex flex-column mb-sm-96 mb-4">
+        <h2 class="ls-2 mb-0 text-sm-end text-center">活力早晨，從這開始！</h2>
+        <h2
+          class="fs-1 text-secondary font-monospace text-uppercase text-sm-end text-center"
+        >
           start from here
-        </h1>
+        </h2>
       </div>
       <ul class="row list-unstyled">
-        <li class="col">
+        <li class="col mb-4 text-center">
           <a href="#" class="btn mw-416">
-            <div class="card border-0 bg-transparent align-items-center">
-              <div class="card-header position-relative mb-32 border-0 bg-transparent">
+            <div class="card border-0 align-items-center">
+              <div class="card-header position-relative mb-4 border-0">
                 <div class="border-top-104"></div>
                 <button
                   type="button"
-                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-2 px-7"
                 >
                   招牌漢堡
                 </button>
               </div>
-              <div class="rounded rounded-5 mb-32">
+              <div class="rounded rounded-7 mb-4">
                 <img
                   src="/src/assets/images/product-1.jpg"
-                  class="img-fluid object-top rounded-5"
+                  class="img-fluid object-top rounded-7"
                   alt="Hamburger"
                 />
               </div>
               <div class="card-body">
-                <p class="text-center px-24">
+                <p class="text-center px-3 mb-0">
                   最經典的美味，小時候老爸都會說，去找你阿母拿一個漢堡蛋當早餐
                 </p>
+              </div>
+              <div class="card-footer border-0 p-0">
                 <div class="border-bottom-104"></div>
               </div>
             </div>
           </a>
         </li>
-        <li class="col">
+        <li class="col mb-4 text-center">
           <a href="#" class="btn mw-416">
-            <div class="card border-0 bg-transparent align-items-center">
-              <div class="card-header position-relative mb-32 border-0 bg-transparent">
+            <div class="card border-0 align-items-center">
+              <div class="card-header position-relative mb-4 border-0">
                 <div class="border-top-104"></div>
                 <button
                   type="button"
-                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-2 px-7"
                 >
-                嚴選套餐
+                  嚴選套餐
                 </button>
               </div>
-              <div class="rounded rounded-5 mb-32">
+              <div class="rounded rounded-7 mb-4">
                 <img
                   src="/src/assets/images/product-2.jpg"
-                  class="img-fluid object-top rounded-5"
+                  class="img-fluid object-top rounded-7"
                   alt="Hamburger"
                 />
               </div>
               <div class="card-body">
-                <p class="text-center px-24">
-                  這個也想吃、那個也想吃？那就來個套餐滿足慾望
-                </p>
+                <p class="text-center px-3 mb-0">這個也想吃、那個也想吃？</p>
+              </div>
+              <div class="card-footer border-0 p-0">
                 <div class="border-bottom-104"></div>
               </div>
             </div>
           </a>
         </li>
-        <li class="col">
+        <li class="col mb-4 text-center">
           <a href="#" class="btn mw-416">
-            <div class="card border-0 bg-transparent align-items-center">
-              <div class="card-header position-relative mb-32 border-0 bg-transparent">
+            <div class="card border-0 align-items-center">
+              <div class="card-header position-relative mb-4 border-0">
                 <div class="border-top-104"></div>
                 <button
                   type="button"
-                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-16 px-64"
+                  class="btn btn-secondary fw-bold fs-3 text-nowrap rounded-pill position-absolute bottom-0 start-50 translate-middle-x py-2 px-7s"
                 >
-                滑嫩蛋餅
+                  滑嫩蛋餅
                 </button>
               </div>
-              <div class="rounded rounded-5 mb-32">
+              <div class="rounded rounded-7 mb-4">
                 <img
                   src="/src/assets/images/product-3.jpg"
-                  class="img-fluid object-top rounded-5"
+                  class="img-fluid object-top rounded-7"
                   alt="Hamburger"
                 />
               </div>
               <div class="card-body">
-                <p class="text-center px-24">
+                <p class="text-center px-3 mb-0">
                   傳統的蛋餅皮，換上Q彈滑嫩的河粉餅皮，蹦出新滋味！
                 </p>
+              </div>
+              <div class="card-footer border-0 p-0">
                 <div class="border-bottom-104"></div>
               </div>
             </div>
@@ -152,36 +181,81 @@
     </section>
   </div>
   <!-- make life smoother -->
-  <section class="mt-200 position-relative">
-    <div class="d-flex justify-content-end">
+  <section class="mt-200 position-relative mt-sm-80">
+    <div class="d-flex justify-content-md-end">
       <img
         src="/src/assets/images/hero2.png"
         style="margin-right: 110.8px"
+        class="d-md-inline d-none float-end img-fluid"
         alt=""
       />
+      <div class="d-md-none">
+        <div class="container">
+          <h2 class="ls-2 mb-0 text-sm-end text-center">
+            活力早晨，從這開始！
+          </h2>
+          <h2
+            class="fs-1 text-secondary font-monospace text-uppercase text-sm-end text-center"
+          >
+            start from here
+          </h2>
+        </div>
+        <img src="/src/assets/images/hero2-m.png" class="w-100 mb-3" alt="" />
+        <div class="container text-center">
+          <router-link
+            to="/cart"
+            class="btn btn-secondary rounded-pill fs-6 ls-3 px-4 py-2 nav-item"
+            role="button"
+          >
+            <img
+              src="/src/assets/images/icon-dice.png"
+              style="height: 32px"
+              class="me-1 hover-hide"
+              alt=""
+              srcset=""
+            />
+            <img
+              src="/src/assets/images/icon-dice-hover.png"
+              style="height: 32px"
+              class="me-1 hover"
+              alt=""
+              srcset=""
+            />
+            讓我幫你選早餐吧
+          </router-link>
+        </div>
+      </div>
     </div>
-    <div class="w-100 h-100 position-absolute top-0 start-0">
+    <div class="w-100 h-100 position-absolute top-0 start-0 d-none d-md-inline">
       <div class="container h-100">
         <div class="row h-100 align-items-center">
           <div class="col-5">
-            <h2 class="mb-8 ls-2">你的煩腦，</h2>
+            <h2 class="mb-1 ls-2">你的煩腦，</h2>
             <h2 class="mb-0 ls-2">我也許能幫忙！</h2>
             <h1
-              class="text-secondary text-nowrap font-monospace text-uppercase mb-48"
+              class="text-secondary text-nowrap font-monospace text-uppercase mb-6"
             >
               make life smoother
             </h1>
-            <p class="fs-6 ls-15 mb-48">
+            <p class="fs-6 ls-15 mb-6">
               每天早上都要煩惱吃什麼嗎？除了日復一日的餐點，來試試樂透的早餐吧！
             </p>
             <router-link
               to="/cart"
-              class="btn btn-secondary rounded-pill fs-6 ls-3 px-32 py-16"
+              class="btn btn-secondary rounded-pill fs-6 ls-3 px-4 py-2 nav-item"
               role="button"
             >
               <img
                 src="/src/assets/images/icon-dice.png"
-                class="me-8"
+                style="height: 32px"
+                class="me-1 hover-hide"
+                alt=""
+                srcset=""
+              />
+              <img
+                src="/src/assets/images/icon-dice-hover.png"
+                style="height: 32px"
+                class="me-1 hover"
                 alt=""
                 srcset=""
               />
@@ -192,29 +266,44 @@
       </div>
     </div>
   </section>
-  <section class="mt-200 container">
-    <div class="row justify-content-between">
-      <div class="col p-88 bg-third rounded-5">
-        <h2 class="ls-2 mb-0">造訪我們</h2>
-        <h1 class="text-danger font-monospace text-uppercase mb-40">
+  <!-- visit us -->
+  <section class="mt-200 container mt-sm-80">
+    <div class="d-flex justify-content-md-between flex-column flex-md-row">
+      <div class="w-50 p-88 bg-third rounded-md-8 rounded-6 text-md-start text-center mb-2 mb-md-0 me-md-3">
+        <h2 class="ls-2 mb-md-0 mb-1">造訪我們</h2>
+        <h1 class="text-danger font-monospace text-uppercase mb-md-5 mb-3">
           visit us
         </h1>
-        <div class="d-flex justify-content-between align-items-center mb-40">
-          <span class="border-start-20"></span>
-          <p class="fs-3 px-24 mb-0 ls-2 fw-bold">鮮堡漢堡 板橋文化店</p>
-          <span class="border-end-20"></span>
-        </div>
-        <div class="d-flex align-items-center mb-16">
-          <span class="material-symbols-rounded me-16"> call </span>
-          <p class="ls-15 mb-0">02-22590910</p>
-        </div>
-        <div class="d-flex align-items-center">
-          <span class="material-symbols-rounded me-16"> location_on </span>
-          <p class="ls-15 mb-0">新北市板橋區文化路一段270巷3弄18號1樓</p>
-        </div>
+        <ul class="list-unstyled text-start mb-0">
+          <li class="d-flex justify-content-between align-items-center mb-5">
+            <span class="border-start-20"></span>
+            <p class="fs-3 px-md-3 px-2 mb-0 ls-2 fw-bold">
+              鮮堡漢堡 板橋文化店
+            </p>
+            <span class="border-end-20"></span>
+          </li>
+          <li class="d-flex align-items-center mb-2">
+            <span class="material-symbols-rounded me-2"> call </span>
+            <p class="ls-15 mb-0">02-22590910</p>
+          </li>
+          <li class="d-flex align-items-center">
+            <span class="material-symbols-rounded me-2"> location_on </span>
+            <p class="ls-15 mb-0">新北市板橋區文化路一段270巷3弄18號1樓</p>
+          </li>
+        </ul>
       </div>
-      <div class="col rounded-5">
-        <img src="/src/assets/images/map.png" alt="" />
+      <div class="w-50 rounded-md-8 rounded-6" style="min-height: 376px; min-width: 351px;">
+        <img
+          src="/src/assets/images/map.png"
+          class="d-none d-md-inline"
+          alt=""
+        />
+        <img
+          src="/src/assets/images/map.png"
+          class="d-md-none h-100 w-100"
+          style="object-fit:cover;"
+          alt=""
+        />
       </div>
     </div>
   </section>
