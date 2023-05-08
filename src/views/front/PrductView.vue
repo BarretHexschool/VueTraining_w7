5465d08249372c3f010afec3e194567ea68ff750
Barret <jianhong.liou@gmail.com>
2023-05-09 00:53:31 +0800

    deploy-fix single product page

--- src/views/front/PrductView.vue
+++ src/views/front/PrductView.vue
@@ -7,13 +7,12 @@
         <img :src="product.imageUrl" class="img-fluid" alt="" />
         <div>
           <div class="">
-    <h2 class="mb-2">{{ product.mainTitle }}</h2>
-    <h3>{{ product.description }}</h3>
-    <p>{{ product.price }} 元</p>
-  </div>
-  <template v-if="product.is_select===1">
-    <VForm ref="form" @submit="addToCartBtn">
-            <div v-if="groupProduct.select1" class="mb-2 radioBox">
+            <h2 class="mb-2">{{ product.mainTitle }}</h2>
+            <h3>{{ product.description }}</h3>
+            <p>{{ product.price }} 元</p>
+          </div>
+          <VForm ref="form" @submit="addToCartBtn">
+            <div v-if="product.select1" class="mb-2 radioBox">
               <p class="mb-0">主食選擇：</p>
               <ErrorMessage name="selectedOption1"
                 ><p class="invalid-feedback d-block my-0">
@@ -25,17 +24,18 @@
                 id="selectedOption1"
                 name="selectedOption1"
                 as="select"
-                class="form-select" aria-label="Default select"
+                class="form-select"
+                aria-label="Default select"
                 rules="required"
                 v-model="selectedOption1"
               >
-              <template v-for:="(option) in groupProduct.select1">
-                <option :value="option">{{option}}</option>
-              </template>
+                <template v-for:="option in product.select1">
+                  <option :value="option">{{ option }}</option>
+                </template>
               </Field>
             </div>
 
-            <div v-if="groupProduct.select2" class="mb-2 radioBox">
+            <div v-if="product.select2" class="mb-2 radioBox">
               <p class="mb-0">飲料選擇：</p>
               <ErrorMessage name="selectedOption2"
                 ><p class="invalid-feedback d-block my-0">
@@ -46,16 +46,17 @@
                 id="selectedOption2"
                 name="selectedOption2"
                 as="select"
-                class="form-select" aria-label="Default select"
+                class="form-select"
+                aria-label="Default select"
                 rules="required"
                 v-model="selectedOption2"
               >
-              <template v-for:="(option) in groupProduct.select2">
-                <option :value="option">{{option}}</option>
-              </template>
+                <template v-for:="option in product.select2">
+                  <option :value="option">{{ option }}</option>
+                </template>
               </Field>
             </div>
-            <div v-if="groupProduct.iceLevel" class="mb-2 radioBox">
+            <div v-if="product.iceLevel" class="mb-2 radioBox">
               <p class="mb-0">冰冷熱選擇：</p>
               <ErrorMessage name="iceLevel"
                 ><p class="invalid-feedback d-block my-0">
@@ -66,26 +67,79 @@
                 id="iceLevel"
                 name="iceLevel"
                 as="select"
-                class="form-select" aria-label="Default select"
+                class="form-select"
+                aria-label="Default select"
                 rules="required"
                 v-model="iceLevel"
               >
-              <template v-for="(option, index) in drinkType" :key="index">
-                <option :value="option">{{option}}</option>
-              </template>
+                <template v-for="(option, index) in drinkType" :key="index">
+                  <option :value="option">{{ option }}</option>
+                </template>
               </Field>
             </div>
             <div class="modal-footer justify-content-between">
-          <button type="submit" class="btn btn-secondary">放入購物車</button>
-        </div>
+              <button
+                type="button"
+                class="btn btn-danger"
+                @click="$router.back(-1)"
+              >
+                回上頁
+              </button>
+              <button type="submit" class="btn btn-secondary">
+                放入購物車
+              </button>
+            </div>
           </VForm>
-  </template>
-  <template v-if="product.is_select!==1">
-    <button type="btn" class="btn btn-secondary" @click="addToCartBtn">放入購物車</button>
-  </template>
         </div>
       </div>
-      {{ product }}
+        <Swiper
+        :breakpoints="swiperBreakpoints"
+        :navigation="false"
+        :loop="true"
+        :autoplay="true"
+        :modules="modules"
+        class="mySwiper"
+        style="padding-top: 120px"
+        >
+        <SwiperSlide
+          v-for:="product in showProducts"
+          class="card position-relative h-100  border-0"
+        >
+          <RouterLink :to="`/product/${product.id}`">
+            <div class="card position-relative border border-2 border-secondary" style="margin-top: 120px">
+              <img
+                :src="product.imageUrl"
+                class="card-img-top position-absolute top-0 start-50 translate-middle img-fluid"
+                :alt="product.title"
+              />
+
+              <div class="card-body">
+                <p class="text-black">{{ product.mainTitle }}</p>
+                <div class="priceBox text-black">
+                  <div
+                    class="fs-5"
+                    v-if="product.price == product.origin_price"
+                  >
+                    ${{ product.price }} 元
+                  </div>
+                  <div
+                    class="d-flex flex-column flex-lg-row align-content-end align-items-end me-1"
+                    v-else
+                  >
+                    <del class="fs-7 me-lg-1">
+                      原價：$ {{ product.origin_price }} 元</del
+                    >
+                    <span class="fs-5 fw-bold"
+                      >特價： $ {{ product.price }} 元</span
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </RouterLink>
+        </SwiperSlide>
+      </Swiper>
+
     </section>
   </main>
 </template>
@@ -95,21 +149,46 @@ import cartStore from '@/stores/useCartStore'
 import sweetAlertStore from '@/stores/useSweetAlertStore'
 import { Field, Form, ErrorMessage } from 'vee-validate'
 import { mapActions } from 'pinia'
-
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { Autoplay, Navigation } from 'swiper'
 const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
 export default {
   data () {
     return {
       product: [],
       products: [],
-      groupProduct: {},
+      showProducts: [],
       isLoading: true,
       title: '鮮堡美食',
       drinkType: ['冰', '冷(去冰)', '溫'],
       message: '',
       selectedOption1: '',
       selectedOption2: '',
-      iceLevel: '冰'
+      iceLevel: '冰',
+      swiperBreakpoints: {
+        0: {
+          slidesPerView: 1.5,
+          spaceBetween: 10
+        },
+        375: {
+          slidesPerView: 1.5,
+          spaceBetween: 20
+        },
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 30
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 50
+        }
+      }
     }
   },
   methods: {
@@ -122,64 +201,82 @@ export default {
           this.title = this.product.mainTitle
           this.selectedOption1 = this.product.select1
           this.selectedOption2 = this.product.select2
-          this.isSetProdcuct()
+          this.getAllProducts()
         })
         .catch((err) => {
           this.swalError(err.response.data.message)
         })
     },
     getAllProducts () {
-      this.$http.get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
+      this.$http
+        .get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
         .then((res) => {
-          this.products = res.data.products
-          this.filterSetProduct(res.data.products)
+          this.products = Object.values(
+            this.gropuSetProducts(res.data.products)
+          )
+          this.showProducts = this.randomArray()
+          this.checkIsSet()
+          this.isLoading = false
         })
     },
-    isSetProdcuct () {
+    checkIsSet () {
       if (this.product.is_select === 1) {
-        this.getAllProducts()
-      } else {
-        this.isLoading = false
+        this.product = this.products.find((item) => {
+          return item.mainTitle === this.title
+        })
       }
     },
-    filterSetProduct (products) {
-      const setProducts = products.filter(product => product.mainTitle === this.product.mainTitle)
-      this.groupProduct = Object.values(this.gropuSetProduct(setProducts))[0]
-      this.isLoading = false
-    },
-    gropuSetProduct (setProducts) {
+    gropuSetProducts (setProducts) {
       const setGroup = {}
       setProducts.forEach((item) => {
         const { mainTitle, select1, select2 } = item
-        if (setGroup[mainTitle] === undefined) {
+        if (item.is_select === 1) {
+          if (setGroup[mainTitle] === undefined) {
+            setGroup[mainTitle] = {
+              ...item
+            }
+          } else {
+            if (select1 !== undefined) {
+              if (typeof setGroup[mainTitle].select1 === 'string') {
+                if (setGroup[mainTitle].select1 !== select1) {
+                  setGroup[mainTitle].select1 = [
+                    setGroup[mainTitle].select1,
+                    select1
+                  ]
+                }
+              } else {
+                if (!this.checkHas(setGroup[mainTitle].select1, select1)) {
+                  setGroup[mainTitle].select1 = [
+                    ...setGroup[mainTitle].select1,
+                    select1
+                  ]
+                }
+              }
+            }
+
+            if (typeof setGroup[mainTitle].select2 === 'string') {
+              if (setGroup[mainTitle].select2 !== select2) {
+                setGroup[mainTitle].select2 = [
+                  setGroup[mainTitle].select2,
+                  select2
+                ]
+              }
+            } else {
+              if (!this.checkHas(setGroup[mainTitle].select2, select2)) {
+                setGroup[mainTitle].select2 = [
+                  ...setGroup[mainTitle].select2,
+                  select2
+                ]
+              }
+            }
+          }
+        } else {
           setGroup[mainTitle] = {
             ...item
           }
-        } else {
-          if (select1 !== undefined) {
-            if (typeof (setGroup[mainTitle].select1) === 'string') {
-              if (setGroup[mainTitle].select1 !== select1) {
-                setGroup[mainTitle].select1 = [setGroup[mainTitle].select1, select1]
-              }
-            } else {
-              if (!this.checkHas(setGroup[mainTitle].select1, select1)) {
-                setGroup[mainTitle].select1 = [...setGroup[mainTitle].select1, select1]
-              }
-            }
-          }
-
-          if (typeof (setGroup[mainTitle].select2) === 'string') {
-            if (setGroup[mainTitle].select2 !== select2) {
-              setGroup[mainTitle].select2 = [setGroup[mainTitle].select2, select2]
-            }
-          } else {
-            if (!this.checkHas(setGroup[mainTitle].select2, select2)) {
-              setGroup[mainTitle].select2 = [...setGroup[mainTitle].select2, select2]
-            }
-          }
         }
       })
-      return (setGroup)
+      return setGroup
     },
     checkHas (arr, find) {
       return arr.includes(find)
@@ -202,14 +299,31 @@ export default {
         this.iceLevel = undefined
       }
       const findProduct = this.products.find((p) => {
-        return (p.mainTitle === this.product.mainTitle &&
-                    p.select1 === this.selectedOption1 &&
-                    p.select2 === this.selectedOption2 &&
-                    p.iceLevel === this.iceLevel)
+        return (
+          p.mainTitle === this.product.mainTitle &&
+          p.select1 === this.selectedOption1 &&
+          p.select2 === this.selectedOption2 &&
+          p.iceLevel === this.iceLevel
+        )
       })
       return findProduct.id
     },
-
+    shuffleArray (array) {
+      for (let i = array.length - 1; i >= 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1))
+        const itemAtIndex = array[randomIndex]
+        array[randomIndex] = array[i]
+        array[i] = itemAtIndex
+      }
+      return array
+    },
+    randomArray () {
+      const random = this.shuffleArray(this.products)
+      if (random.length <= 12) {
+        return random
+      }
+      return random.slice(2, 12)
+    },
     ...mapActions(cartStore, ['addToCart']),
     ...mapActions(sweetAlertStore, ['swalError', 'swalToastTopEnd'])
   },
@@ -220,13 +334,23 @@ export default {
   watch: {
     title (newVal) {
       document.title = `${this.title} | 鮮堡漢堡 文化店`
+    },
+    $route: function () {
+      this.getProduct()
     }
   },
   components: {
     LoadingDesign,
     ErrorMessage,
     VForm: Form,
-    Field
+    Field,
+    Swiper,
+    SwiperSlide
+  },
+  setup () {
+    return {
+      modules: [Autoplay, Navigation]
+    }
   }
 }
 </script>
